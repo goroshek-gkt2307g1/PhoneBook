f5e14a3853a3cb81d75d50b5779e6c85b026ef62
Goroshek <goroshek@example.com>
2026-04-19 00:09:48 +0700

    Добавьте файлы проекта.

---
+++ Models/Contact.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Xml.Linq;
+using PhoneBook.VIewModels;
+
+namespace PhoneBook.Models
+{
+	//модель контакта - отвечает за данные и бизнес-логику валидации
+    public class Contact : ObservableObject
+	{
+		private string _name = string.Empty; //приватное имя
+		private string _phone = string.Empty; //приватный телефон
+
+		//новых контакт с валидацией входных данных
+		public Contact(string name, string phone)
+		{
+			//присваиваем свойства 
+			Name = name;
+			Phone = phone;
+
+			//валидность после установки
+			if (Validate() == false)
+				throw new ArgumentException("Неверные данные");
+		}
+
+		//публичное свойство для доступа к имени с уведом об изменениях
+		public string Name
+		{
+			get => _name;
+			set
+			{
+				_name = value;
+				OnPropertyChanged(nameof(Name));
+			}
+		}
+
+		//публичное свойство для доступа к номеру с уведом об изменениях
+		public string Phone
+		{
+			get => _phone;
+			set
+			{
+				_phone = value;
+				OnPropertyChanged(nameof(Phone));
+			}
+		}
+
+		//статический метод валидации, исп. до создания объекта
+		public static bool IsValid(string name, string phone)
+		{
+			return !string.IsNullOrEmpty(name) && //имя не пустое
+				!string.IsNullOrEmpty(phone) && //номер не пустой
+				phone.StartsWith("+7") && //начинается на +7
+				phone.Length >=12 && phone.Length <13; //12 цифр с кодом
+		}
+
+		//исп. после создания объекта
+		public bool Validate() => IsValid(_name, _phone);
+
+	}
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,87 @@
+﻿using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PhoneBook.VIewModels
+{
+    //связывает view и model
+    public class MainViewModel : ObservableObject
+    {
+        //коллекция контактов
+        public ObservableCollection<Contact> Contacts { get; }
+        private string _name = string.Empty; //приватное поле для временного вводимого имени
+        private string _phone = string.Empty; //приватное поле для временного вводимого телефона
+		private Contact? _selectedContact; //приватное поле для хранения выбранного в датагрид контакта
+
+        //свойство для привязки к текстбокс имени
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+
+		//свойство для привязки к текстбокс номера
+		public string Phone
+        {
+            get => _phone;
+            set => Set(ref _phone, value);
+        }
+
+        //свойство для привязки выбранного объекта в датагрид
+        public Contact? SelectedContact
+        {
+            get => _selectedContact;
+            set => Set(ref _selectedContact, value);
+        }
+
+        //команды
+        public ICommand AddCommand { get; }
+        public ICommand DeleteCommand { get; }
+
+		//конструктор инициализирует коллекцию и команды
+		public MainViewModel()
+        {
+            Contacts = new ObservableCollection<Contact>();
+            AddCommand = new RelayCommand(AddContact, () => CanAddContact()); //создание команды добавления с методом выполнения и проверкой возможности
+            DeleteCommand = new RelayCommand(DeleteContact, () => CanDeleteContact());
+        }
+
+		//метод выполнения команды добавления контакта
+		private void AddContact()
+        {
+            Contact contact = new Contact(Name, Phone);
+            Contacts.Add(contact);
+            Name = string.Empty;
+            Phone = string.Empty;
+        }
+
+		//метод проверки возможности добавления контакта
+		private bool CanAddContact()
+        {
+            return Contact.IsValid(Name, Phone);
+        }
+
+		//метод выполнения команды удаления контакта
+		private void DeleteContact()
+        {
+            if (SelectedContact != null)
+            {
+                Contacts.Remove(SelectedContact);
+            }
+        }
+
+		//метод проверки возможности удаления контакта
+		private bool CanDeleteContact()
+        {
+            if (SelectedContact != null)
+                return true;
+            return false;
+        }
+
+    }
+}
